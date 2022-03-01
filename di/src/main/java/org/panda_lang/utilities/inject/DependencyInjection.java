cb812da3a448cf13b7b3ca4bc315a44ab48fe697
dzikoysk <dzikoysk@dzikoysk.net>
2022-03-01 20:47:03 +0100

    Release 1.5.1

--- di/src/main/java/org/panda_lang/utilities/inject/DependencyInjection.java
+++ di/src/main/java/org/panda_lang/utilities/inject/DependencyInjection.java
@@ -18,7 +18,7 @@ package org.panda_lang.utilities.inject;
 
 public final class DependencyInjection {
 
-    protected static final InjectorFactory INJECTOR_FACTORY = new InjectorFactory();
+    static final InjectorFactory INJECTOR_FACTORY = new InjectorFactory();
 
     /**
      * Create injector
