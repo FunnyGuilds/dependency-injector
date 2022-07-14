1ae87d736a4116499f61bd3b111a6f4c72b1e05d
Peridot <peridot491@protonmail.com>
2022-07-14 15:24:19 +0200

    Support injecting private and protected fields

--- di/src/main/java/org/panda_lang/utilities/inject/FieldsInjector.java
+++ di/src/main/java/org/panda_lang/utilities/inject/FieldsInjector.java
@@ -1,5 +1,8 @@
 package org.panda_lang.utilities.inject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.panda_lang.utilities.inject.annotations.Inject;
 
 import java.lang.reflect.Field;
@@ -17,15 +20,27 @@ public final class FieldsInjector<T> {
     public T newInstance(Object... injectorArgs) throws Throwable {
         T instance = constructorInjector.newInstance(injectorArgs);
 
-        for (Field field : instance.getClass().getFields()) {
+        for (Field field : getAllFields(new ArrayList<>(), instance.getClass())) {
             if (!field.isAnnotationPresent(Inject.class)) {
                 continue;
             }
+
+            field.setAccessible(true);
 
             field.set(instance, processor.tryFetchValue(processor, new PropertyField(field), injectorArgs));
         }
 
         return instance;
+    }
+
+    private static List<Field> getAllFields(List<Field> fields, Class<?> type) {
+        fields.addAll(Arrays.asList(type.getDeclaredFields()));
+
+        if (type.getSuperclass() != null) {
+            getAllFields(fields, type.getSuperclass());
+        }
+
+        return fields;
     }
 
 }
