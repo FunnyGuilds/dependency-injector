af3cb56fb32dafdb507d435265c9561d9e1d1497
Peridot <peridot491@protonmail.com>
2022-07-18 19:38:14 +0200

    simplify cache

--- di/src/main/java/org/panda_lang/utilities/inject/MethodsCache.java
+++ di/src/main/java/org/panda_lang/utilities/inject/MethodsCache.java
@@ -8,11 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import panda.std.Pair;
 
 final class MethodsCache {
 
     private final Map<Class<?>, List<Method>> cachedMethods = new HashMap<>();
-    private final Map<Class<?>, Map<Class<? extends Annotation>, List<Method>>> cachedAnnotatedMethods = new HashMap<>();
+    private final Map<Pair<Class<?>, Class<? extends Annotation>>, List<Method>> cachedAnnotatedMethods = new HashMap<>();
 
     public List<Method> getMethods(Class<?> clazz) {
         List<Method> methods = this.cachedMethods.get(clazz);
@@ -24,19 +25,16 @@ final class MethodsCache {
     }
 
     public List<Method> getAnnotatedMethods(Class<?> clazz, Class<? extends Annotation> annotation) {
-        Map<Class<? extends Annotation>, List<Method>> annotatedMethods = this.cachedAnnotatedMethods.get(clazz);
-        if (annotatedMethods == null) {
-            annotatedMethods = new HashMap<>();
-
-            annotatedMethods.put(annotation, this.getMethods(clazz)
+        List<Method> methods = this.cachedAnnotatedMethods.get(Pair.of(clazz, annotation));
+        if (methods == null) {
+            methods = getAllMethods(new ArrayList<>(), clazz)
                     .stream()
                     .filter(method -> method.isAnnotationPresent(annotation))
-                    .collect(Collectors.toList()));
+                    .collect(Collectors.toList());
 
-            this.cachedAnnotatedMethods.put(annotation, annotatedMethods);
-
+            this.cachedAnnotatedMethods.put(Pair.of(clazz, annotation), methods);
         }
-        return annotatedMethods.get(annotation);
+        return methods;
     }
 
     private static List<Method> getAllMethods(List<Method> methods, Class<?> type) {
